00ffb51b4b9fc74059ed72e643c33f4d3c43af8c
Francisco <jscriptcoder@gmail.com>
2021-10-09 11:11:53 +0200

    wip

---
+++ components/OrderBook.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/OrderBook.module.css'
+
+export default function OrderBook() {
+  return (
+    <div className={styles.container}>
+      Order Book component
+    </div>
+  )
+}
