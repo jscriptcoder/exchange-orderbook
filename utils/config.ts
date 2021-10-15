30d7bacf41c960144bfb2eadfe46a5788e8b8c88
Francisco <jscriptcoder@gmail.com>
2021-10-15 10:43:30 +0200

    Fix ws on prod

--- utils/config.ts
+++ utils/config.ts
@@ -1,8 +1,10 @@
 import markets, { MarketInfo } from './markets'
 
 export const port: number = parseInt(process.env.PORT || '3000', 10)
-export const wsProtocol = process.env.NODE_ENV === 'production' ? 'wss' : 'ws'
-export const wsUrl: string = `${wsProtocol}://0.0.0.0:${port}`
+export const prod: boolean = process.env.NODE_ENV === 'production'
+export const wsProtocol = prod ? 'wss' : 'ws'
+export const domain = prod ? 'exchange-orderbook.vercel.app' : 'localhost' // TODO: there are actually 3 domains
+export const wsUrl: string = `${wsProtocol}://${domain}:${port}`
 
 export const orderBookProtocol: string = 'order-book'
 export const defaultMarket: MarketInfo = markets.PI_XBTUSD
